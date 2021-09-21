cee4107de3e79c975746beda8f25b950a77e9d8b
Rashik4567 <rashikbinsayeed4567@gmail.com>
2021-09-21 19:48:46 +0600

    Fixed issue #4 #2 .

--- __main__.py
+++ __main__.py
@@ -1,7 +1,21 @@
 import os
 import platform
 
+# this can run programme in parallal
+''' ~~linux~~
+some_command &
+P1=$!
+other_command &
+P2=$!
+wait $P1 $P2~
+
+~~windows~~ 
+(adding /b will stop creating new window of cmd.)
+start "" first_comamnd
+start "" second_command
+'''
+
 if platform.system().lower() == "windows":
-    print(os.system("python manage.py runserver"))
+    print(os.system('''start "backend" python manage.py runserver && cd frontend && start "frontend" npm start'''))
 else:
-    print(os.system("python3 manage.py runserver"))
+    print(os.system("python3 manage.py runserver & P1=$! (cd frontend && npm start) & P2=$! wait $P1 $P2"))
